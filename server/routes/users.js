22a195945c614cc6cbdf8e1a569b769205e6f22b
Ravindra-Patel <ravindra.patel@bluepineapple.io>
2023-06-15 17:28:51 +0530

    Backend server code is ready

---
+++ server/routes/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+//verfiytoken is a middle ware to check whether the user os verfied or not.
+//If yes then call the getUer controller to perform the business logic
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
